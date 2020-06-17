24d259ec6bd55cd33fa7902a43d5529e57b2c656
Giovani Garfias <46204471+survani@users.noreply.github.com>
2020-06-16 20:03:39 -0700

    Removed form because it was useless.

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,19 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import Header from './header/Header';
 import ProfileContext from '../../contexts/ProfileContext';
 import { axiosWithAuth } from '../authentication/axiosWithAuth';
 import { HeaderSection } from './style';
 import { decode } from 'jsonwebtoken';
 
-const initialUserInfo = {
-  name: '',
-  location: '',
-  bio: '',
-};
-
 export default function Profile() {
-  const [userInfo, setUserInfo] = useState(initialUserInfo);
+  const [userInfo, setUserInfo] = useState({});
 
   const getUserInfo = () => {
     //provides the info for the specific user that is logged in.
@@ -33,69 +26,10 @@ export default function Profile() {
     getUserInfo();
   }, []);
 
-  const postUserInfo = (user) => {
-    axios
-      .post('', user)
-      .then((res) => {
-        setUserInfo(res.data, ...userInfo);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-      .finally(() => {
-        setUserInfo(initialUserInfo);
-      });
-  };
-
-  const handleChanges = (evt) => {
-    const { name } = evt.target;
-    const { value } = evt.target;
-
-    setUserInfo({
-      ...userInfo,
-      [name]: value,
-    });
-  };
-
-  const onSubmit = (evt) => {
-    evt.preventDefault();
-
-    const user = {
-      image: userInfo.image.trim(),
-      location: userInfo.location.trim(),
-      bio: userInfo.bio.trim(),
-    };
-    postUserInfo(user);
-  };
-
   return (
     <ProfileContext.Provider value={{ userInfo }}>
       <HeaderSection>
         <Header />
-        {/* <form className="form-container" onSubmit={onSubmit}>
-        <label>Name</label>
-        <input
-        type="text"
-        name="image"
-        onChange={handleChanges}
-        value={userInfo.image}
-        />
-        <label>Location</label>
-        <input
-        type="text"
-        name="location"
-        onChange={handleChanges}
-        value={userInfo.affiliate}
-        />
-        <label>Bio</label>
-        <input
-        type="text"
-        name="bio"
-        onChange={handleChanges}
-        value={userInfo.bio}
-        />
-        <button>Post</button>
-      </form> */}
       </HeaderSection>
     </ProfileContext.Provider>
   );
