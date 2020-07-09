5b94d131b18eaf0108061d14bf0cdf443ae4b00d
Giovani Garfias <giovaniportal@gmail.com>
2020-07-09 12:45:20 -0700

    Finish more testing for LandingPage.js

--- src/components/landingpage/landingpage.test.js
+++ src/components/landingpage/landingpage.test.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import LandingPage from "./LandingPage";
 import { BrowserRouter } from "react-router-dom";
 import "@testing-library/jest-dom/extend-expect";
-import * as rtl from '@testing-library/react'
+import * as rtl from "@testing-library/react";
 
-test("renders the landing page without crashing", () => {
+it("renders the landing page without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
     <BrowserRouter>
@@ -15,20 +15,32 @@ test("renders the landing page without crashing", () => {
   );
 });
 
+it("Checks if the text Get Started is present", () => {
+  const wrapper = rtl.render(
+    <BrowserRouter>
+      <LandingPage />
+    </BrowserRouter>
+  );
+  const element = wrapper.getByText(/Get Started/i);
+  expect(element).toBeInTheDocument();
+});
 
-test('renders dashboard component', () => {
-    const wrapper = rtl.render(<BrowserRouter><LandingPage/></BrowserRouter>)
-    const element = wrapper.getByText(/Get Started/i)
-    expect(element).toBeInTheDocument()
+it("Checks if the text Sign in is present", () => {
+  const wrapper = rtl.render(
+    <BrowserRouter>
+      <LandingPage />
+    </BrowserRouter>
+  );
+  const element = wrapper.getByText(/Sign In/i);
+  expect(element).toBeInTheDocument();
+});
 
-
-})
-
-test('renders dashboard component', () => {
-    const wrapper = rtl.render(<BrowserRouter><LandingPage/></BrowserRouter>)
-    const element = wrapper.getByText(/Sign In/i)
-    expect(element).toBeInTheDocument()
-})
-
-
-
+it("Checks if intro image has appropriate alt text", () => {
+  const image = rtl.render(
+    <BrowserRouter>
+      <LandingPage />
+    </BrowserRouter>
+  );
+  const element = image.getByAltText("Workout");
+  expect(element).toBeInTheDocument();
+});
